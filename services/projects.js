fa6720776f3f85c71622d288a21b86b1a2c1c4c3
sebastian-lucjan <sebastian.lucjan@gmail.com>
2021-09-06 16:35:40 +0200

    Parse markdown files, add styles

---
+++ services/projects.js
@@ -0,0 +1,9 @@
+import { getList } from 'lib/markdownParser';
+
+export const getAllProjects = () => {
+  const projects = getList('_projects');
+
+  return {
+    props: { projects: projects.sort((a, b) => b.createAt - a.createAt) }
+  };
+};
